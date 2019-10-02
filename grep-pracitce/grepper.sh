a7e2a9ab44704b5c20e921098cbd268f4106840d
NewEnclave17 <54423470+NewEnclave17@users.noreply.github.com>
2019-10-02 13:11:21 -0500

    Delete grepper.sh

--- grep-pracitce/grepper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-grep -oh "\w*ion* | grep e > ion.txt
-grep -oh "\w*ation\ | grep e > ation.txt
-grep -oh [0-9] | grep e > numbers.txt
-grep -oh \w*ed\w* |grep e > ed.txt
-grep ^[A-Z].*[.!}' | grep e > sentences.txt
